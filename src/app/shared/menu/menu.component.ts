1e92412b1c73f4bc78fff5061c6b9c8a15d0ce48
Santiago <santtiago49@gmail.com>
2022-02-28 12:15:54 -0300

    mune de la aplicacion

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem { 
+  ruta: string;
+  texto: string;
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
+    `
+    li{
+      cursor: pointer;
+    }
+    `
   ]
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
+
+  menu: MenuItem[] = [
+    {
+      ruta: 'graficas/barra',
+      texto: 'Barra'
+    },
+    {
+      ruta: 'graficas/barra-doble',
+      texto: 'Barra Doble'
+    },{
+      ruta: 'graficas/dona',
+      texto: 'Dona'
+    },{
+      ruta: 'graficas/dona-http',
+      texto: 'Dona http'
+    },
+  ]
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
