10be00cc916f23f7477248a3b259206f41ad4610
Santiago <santtiago49@gmail.com>
2022-03-02 10:24:20 -0300

    Grafica de barra

--- src/app/graficas/graficas.module.ts
+++ src/app/graficas/graficas.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgChartsModule } from 'ng2-charts';
+
 import { GraficasRoutingModule } from './graficas-routing.module';
 import { BarrasComponent } from './pages/barras/barras.component';
 import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
 import { DonaHttpComponent } from './pages/dona-http/dona-http.component';
 import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.component';
 import { DonaComponent } from './components/dona/dona.component';
+
 
 
 @NgModule({
@@ -19,7 +22,8 @@ import { DonaComponent } from './components/dona/dona.component';
   ],
   imports: [
     CommonModule,
-    GraficasRoutingModule
+    GraficasRoutingModule,
+    NgChartsModule
   ]
 })
 export class GraficasModule { }
--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+
 
 @Component({
   selector: 'app-barras',
@@ -6,11 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BarrasComponent implements OnInit {
+export class BarrasComponent {
 
-  constructor() { }
+  
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
-  ngOnInit(): void {
+  barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+  }
+
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartData: ChartData<'bar'> = {
+    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+    datasets: [
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#D94E5A', hoverBackgroundColor: 'red' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor: '#034AA6', hoverBackgroundColor: 'red' },
+      { data: [ 28, 48, 40, 19, 86, 27, 2  ], label: 'Series C', backgroundColor: '#05DBF2', hoverBackgroundColor: 'red' }
+    ]
+  };
+  public randomize(): void {
+    // Only Change 3 values
+    this.barChartData.datasets[2].data = [
+      0, 0, 0, 0, 0, 0, 0,
+    ];
+    this.chart?.update();
   }
 
 }
