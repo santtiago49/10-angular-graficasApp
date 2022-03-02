a85674247b754e36c2fbec177e885823d6ca4092
Santiago <santtiago49@gmail.com>
2022-03-02 13:40:34 -0300

    Grafica de dona

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData, ChartType, Color } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -6,11 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
+
+  public colors: Color[] = [
+    '#8ecae6',
+    '#219ebc',
+    '#023047',
+    '#ffb703',
+    '#fb8500'
+  ]
+
+  public hoverColors: Color[] = [
+    '#6A91F7', 
+    '#6A9AF7', 
+    '#6A90F7'
+  ]
+   
+
+  public doughnutChartLabels: string[] = [ 
+    'Download Sales', 'In-Store Sales', 'Mail-Order Sales' 
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+    { 
+      data: [ 350, 450, 100, 94],
+      backgroundColor: this.colors,
+      hoverBackgroundColor: this.hoverColors
+    }
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
