cc47fca30273c2e99ba6e0f6388b7236d97c4985
Santiago <santtiago49@gmail.com>
2022-03-02 21:29:10 -0300

    mostrar informacion en la grafica

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
 import { GraficasService } from '../../services/graficas.service';
 
 @Component({
@@ -9,12 +10,24 @@ import { GraficasService } from '../../services/graficas.service';
 })
 export class DonaHttpComponent implements OnInit {
 
+  public doughnutChartLabels: string[] = []; 
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: [],
+    datasets: [{data: []}]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
   constructor( private graficasService: GraficasService) { }
 
   ngOnInit(): void {
     this.graficasService.getUsuariosRedesSociales()
-        .subscribe( resp => {
-          console.log( resp )
+        .subscribe( data => {
+          const labels = Object.keys(data)
+
+          this.doughnutChartData = {
+            labels: labels,
+            datasets: [ {data: Object.values(data)}]
+          }
         })
   }
 
